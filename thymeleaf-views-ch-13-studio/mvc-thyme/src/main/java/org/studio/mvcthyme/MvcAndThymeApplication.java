a7a5b1ebd200dac0c3f4d087262e322f3d340d6c
AJmints <alexjmints@gmail.com>
2023-10-02 21:49:22 -0500

    Studio 13 solution

---
+++ thymeleaf-views-ch-13-studio/mvc-thyme/src/main/java/org/studio/mvcthyme/MvcAndThymeApplication.java
@@ -0,0 +1,13 @@
+package org.studio.mvcthyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcAndThymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcAndThymeApplication.class, args);
+	}
+
+}
---
+++ thymeleaf-views-ch-13-studio/mvc-thyme/src/test/java/org/studio/mvcthyme/MvcAndThymeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.studio.mvcthyme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcAndThymeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
